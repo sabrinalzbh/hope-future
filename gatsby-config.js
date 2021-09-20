77c94351695bb42c3ce41e5cc5e8edbbd72d34a4
Sabrina Chacon <sabrinachacon@Sabrinas-MacBook-Air.local>
2021-09-20 18:51:04 -0400

    part 5 done-MDX

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,13 +5,14 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
+    "gatsby-plugin-sharp", 
     {
       resolve: "gatsby-source-filesystem",
       options: {
         name: `blog`,
         path: `${__dirname}/blog`,
       }
-    }
+    },
+    "gatsby-plugin-mdx",
   ],
 };
